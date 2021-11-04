3fce70aaaba881933c5fed728e0ff863aacff650
PyaePhyoAung-web <80073839+PyaePhyoAung-web@users.noreply.github.com>
2021-11-05 00:41:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-const http = require('http');
+import http from 'http';
 var port = process.env.PORT || 8000;
-http.Server((req, res) => {
-    if (req.url === '/result4/') {
-      let CORS = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
-      };
-      const result = {
-        message: 'pyaephyoaung',
-        'x-result': req.headers['x-test'],
-      };
-      let body = '';
-      req
-        .on('data', data => (body += data))
-        .on('end', () => {
-          result['x-body'] = body;
-          res.writeHead(200, {... CORS, 'Content-Type': 'application/json' });
-          res.end(JSON.stringify(result));
-        });
-    }
-  })
+const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
+  };
+const server = http.createServer(async(req,res)=>{
+      if(req.url === '/result4/'){
+          
+          res.writeHead(200,{
+              'Content-Type':'application/json',
+              ...CORS,
+          })
+          let data = '';
+          await req.on('data', function(chunk){
+            data += chunk;
+            }).on('end', () => {
+          })
+          res.write(JSON.stringify({
+            "message":"pyaephyoaung",
+            "x-result":res.headers['x-test'] ?? '',
+            "x-body":data
+            }
+            ))
+      }
+      res.end()
+      
+  });
   server.listen(port,()=>{
       console.log('Server is running')});
