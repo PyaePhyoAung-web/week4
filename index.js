56c18234357e2fe76229549a0aca46d3250300e3
PyaePhyoAung-web <80073839+PyaePhyoAung-web@users.noreply.github.com>
2021-11-05 00:30:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 const http = require('http');
-
-http
-  .Server((req, res) => {
-
+http.Server((req, res) => {
     if (req.url === '/result4/') {
       let CORS = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
-
       };
-
       const result = {
         message: 'pyaephyoaung',
         'x-result': req.headers['x-test'],
       };
       let body = '';
-
       req
         .on('data', data => (body += data))
         .on('end', () => {
